00dac6475703f5d4ca8a97923f4cf09414a7a4bb
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2022-09-01 14:57:39 -0300

    hola

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const server = require("./src/app");
+const dotenv = require('dotenv')
+dotenv.config()
 
 
-server.listen(process.env.PORT | 3001, () => {
+server.listen(process.env.PORT || 3001, () => {
   console.log("Best Quotes are waiting at port 3001");
 });
